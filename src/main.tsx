b64b2b45bd71098a562a0d659024995c8c6b9d28
GABRYEL FILLIPE CAVALCANTI DA SILVA <25132404@dsn127m1242374.cfp127aluno.local>
2026-05-07 11:01:22 -0300

    feat: Implementando integração local para a tela de registro e login de usuarios

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { AppRoutes } from "./routes.tsx";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AppRoutes />
-  </StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <AppRoutes />
+    </QueryClientProvider>
+  </StrictMode>,
 );
---
+++ src/services/auth/auth.service.ts
@@ -0,0 +1,55 @@
+import { api } from "../api";
+
+export interface LoginData {
+  email: string;
+  password: string;
+}
+
+export interface LoginResponse {
+  development: string;
+  api_description: string;
+  request_date: string;
+  response: {
+    user: Array<{
+      id_guardian: number;
+      guardian_name: string;
+      email: string;
+      profile_picture: string;
+      active: number;
+      token: string;
+    }>;
+  };
+  status: boolean;
+  status_code: number;
+}
+
+export interface RegisterData {
+  email: string;
+  guardian_name: string;
+  password: string;
+}
+
+export interface RegisterResponse {
+  development: string;
+  api_description: string;
+  request_date: string;
+  response: {
+    email: string;
+    guardian_name: string;
+    password: string;
+  };
+  status: boolean;
+  status_code: number;
+}
+
+export const loginService = async (data: LoginData): Promise<LoginResponse> => {
+  const response = await api.post<LoginResponse>("/auth/user", data);
+  return response.data;
+};
+
+export const registerService = async (
+  data: RegisterData,
+): Promise<RegisterResponse> => {
+  const response = await api.post<RegisterResponse>("/user", data);
+  return response.data;
+};
